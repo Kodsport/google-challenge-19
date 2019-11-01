1a9b6f9af9ea7f6bc869af8d58fb83aa7a39492e
Simon Lindholm <simon.lindholm10@gmail.com>
2019-11-01 14:31:43 +0100

    Don't use one-char filenames

--- dictionary/submissions/too_large/sl.py
+++ dictionary/submissions/too_large/sl.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import bz2,os
 w=b''
-for x in bz2.open(__file__[:-5]+'c').read().split(b'%'):
+for x in bz2.open(__file__[:-3]).read().split(b'%'):
  if x:w=w[:len(w)-x[0]]+x[1:]
  else:w+=b"'s"
  print(w.decode())
